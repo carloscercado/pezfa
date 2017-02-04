6bdb532b9f20a65a92c795e09d0b592d64e864b1
Meritzabeth Yegres <merityegres93@gmail.com>
2017-02-04 12:20:17 -0430

    Main trabajo...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,23 @@
  *
  * @author Adela Hernandez
  */
+import java.util.*;
+import com.pezfa.inventario.database.AlmacenDB;
+import com.pezfa.inventario.models.Almacen;
+import javax.swing.JOptionPane;
 public class Main
 {
     public static void main (String arg[])
     {
+        Almacen obj = new Almacen (0, "Santa Rosa", "Santa Rosa Cumaná", "0293-41629053", null);
+        if(AlmacenDB.create(obj))
+        {
+            JOptionPane.showMessageDialog(null, "Registrado Correctamente");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Problemas Al Registrar");
+        }
         
     }
 }
