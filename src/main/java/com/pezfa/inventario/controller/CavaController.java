27057a6f8b01ec92be4f55d6e393e4addadec6b0
Meritzabeth Yegres <merityegres93@gmail.com>
2017-03-05 15:29:42 -0430

    Trabajo del Eliminar del Crud

--- src/main/java/com/pezfa/inventario/controller/CavaController.java
+++ src/main/java/com/pezfa/inventario/controller/CavaController.java
@@ -2,10 +2,14 @@ package com.pezfa.inventario.controller;
 
 import com.pezfa.inventario.database.CavaDB;
 import com.pezfa.inventario.models.Cava;
+import com.pezfa.inventario.models.Producto;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
 
 @ManagedBean
 @ViewScoped
@@ -42,21 +46,15 @@ public class CavaController implements Serializable
     {
         if(CavaDB.create(cava))
         {
-            System.out.println("Registrado Exitosamente");
+            cava = new Cava();
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro existoso", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+            RequestContext con = RequestContext.getCurrentInstance();
+            con.execute("PF('registrar').hide();");
         }else
         {
-            System.out.println("No Se Pudo Registrar");
-        }
-    }
-    
-    public void delete()
-    {
-        if(CavaDB.delete(cava))
-        {
-            System.out.println("Eliminado Exitosamente");
-        }else
-        {
-            System.out.println("No Se Pudo Eliminar");
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Registro fallido", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
         }
     }
     
@@ -68,6 +66,22 @@ public class CavaController implements Serializable
         }else
         {
             System.out.println("No Se Pudo Actualizar");
+        }
+    }
+    
+    public void delete()
+    {
+        if(CavaDB.delete(cava))
+        {
+            cava = new Cava();
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Eliminado exitosamente", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+            RequestContext con = RequestContext.getCurrentInstance();
+            con.execute("PF('eliminar').hide();");
+        }else
+        {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_WARN, "Problemas al eliminar", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
         }
     }
 }
