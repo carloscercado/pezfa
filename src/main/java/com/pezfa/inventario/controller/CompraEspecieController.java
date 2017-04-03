26c99f8de2fee3d2f80b12f3b3e4ad19d45711e2
Carlos Cercado <cercadocarlos@gmail.com>
2017-04-02 23:04:06 -0400

    Trabajo de efrain

--- src/main/java/com/pezfa/inventario/controller/CompraEspecieController.java
+++ src/main/java/com/pezfa/inventario/controller/CompraEspecieController.java
@@ -3,26 +3,56 @@ package com.pezfa.inventario.controller;
 import com.pezfa.inventario.database.CompraEspecieDB;
 import com.pezfa.inventario.models.CompraEspecie;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.context.RequestContext;
 
 @ManagedBean
 @ViewScoped
 public class CompraEspecieController implements Serializable
 {
+
     private CompraEspecie compraEspecie = null;
-   private List<CompraEspecie> compraEspecies = null;
+    private List<CompraEspecie> compraEspecies = null;
+    private List<CompraEspecie> miLista = null;
+    @ManagedProperty(value = "#{compraController}")
+    private CompraController compraController;
 
     public CompraEspecieController()
     {
-        compraEspecie = new CompraEspecie(); 
+        compraEspecie = new CompraEspecie();
+        miLista = new ArrayList<CompraEspecie>();
+    }
+
+    public CompraController getCompraController()
+    {
+        return compraController;
+    }
+
+    public void setCompraController(CompraController compraController)
+    {
+        this.compraController = compraController;
+    }
+
+    
+    public List<CompraEspecie> getMiLista()
+    {
+        return miLista;
+    }
+
+    public void setMiLista(List<CompraEspecie> miLista)
+    {
+        this.miLista = miLista;
     }
 
     public CompraEspecie getCompraEspecie()
     {
         return compraEspecie;
     }
+
     public void setCompraEspecie(CompraEspecie compraEspecie)
     {
         this.compraEspecie = compraEspecie;
@@ -33,6 +63,7 @@ public class CompraEspecieController implements Serializable
         compraEspecies = CompraEspecieDB.read();
         return compraEspecies;
     }
+
     public void setCompraEspecies(List<CompraEspecie> compraEspecies)
     {
         this.compraEspecies = compraEspecies;
@@ -40,13 +71,29 @@ public class CompraEspecieController implements Serializable
 
     public void register()
     {
-        if (CompraEspecieDB.create(compraEspecie))
+        if (CompraEspecieDB.createList(miLista))
         {
             System.out.println("Registrado");
         } else
         {
             System.out.println("No Registrado");
         }
+    }
+    
+    public void add()
+    {
+        compraEspecie.setCompra(compraController.getCompra());
+        miLista.add(compraEspecie);
+        compraEspecie = new CompraEspecie();
+        RequestContext con = RequestContext.getCurrentInstance();
+        con.execute("PF('agregar').hide(); PF('registrar').hide();");
+        
+    }
+    
+    public void remove()
+    {
+        miLista.remove(compraEspecie);
+        compraEspecie = new CompraEspecie();        
     }
 
     public void delete()
---
+++ src/main/java/com/pezfa/inventario/converter/ProveedorConverter.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pezfa.inventario.converter;
+
+import com.pezfa.inventario.database.ProveedorDB;
+import com.pezfa.inventario.models.Proveedor;
+import java.util.List;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.view.Location;
+
+/**
+ *
+ * @author Alicia
+ */
+@FacesConverter(forClass = Location.class, value = "proveedor")
+public class ProveedorConverter implements Converter {
+
+    public static List<Proveedor> proveedors;
+
+    public ProveedorConverter() {
+        proveedors = ProveedorDB.read();
+    }
+
+    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+        Integer code =  Integer.parseInt(value);
+        Proveedor obj = buscar(code);
+        return obj;
+        
+    }
+
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if(o==null||o.equals(""))
+        {
+            return "";
+        }else
+        {
+            return String.valueOf(((Proveedor)o).getId());
+        }
+    }
+    
+    public Proveedor buscar(int id)
+    {
+        for (Proveedor obj : proveedors)
+        {
+            if(obj.getId()==id)
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+}
