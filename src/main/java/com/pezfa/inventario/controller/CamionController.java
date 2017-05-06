eeb1f9ce18406fff90f098be709212e13615cde5
Carlos Cercado <cercadocarlos@gmail.com>
2017-05-06 16:21:50 -0400

    creacion del historial de compra, actualizacion db

---
+++ src/main/java/com/pezfa/inventario/controller/CamionController.java
@@ -0,0 +1,104 @@
+package com.pezfa.inventario.controller;
+
+import com.pezfa.inventario.database.AlmacenDB;
+import com.pezfa.inventario.models.Almacen;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
+
+@ManagedBean
+@ViewScoped
+public class AlmacenController implements Serializable
+{
+
+    private Almacen almacen = null; // objeto a controlar
+    private List<Almacen> almacenes = null; // lista de objetos de tipo almace
+    private AlmacenDB db;
+
+    //constructor
+    public AlmacenController()
+    {
+        almacen = new Almacen(); //instancio el objeto almacen
+        db = new AlmacenDB();
+    }
+
+    //getter y setter
+    public Almacen getAlmacen()
+    {
+        return almacen;
+    }
+
+    public void setAlmacen(Almacen almacen)
+    {
+        this.almacen = almacen;
+    }
+
+    public List<Almacen> getAlmacenes()
+    {
+        almacenes = db.read("from Almacen");
+        return almacenes;
+    }
+
+    public void setAlmacenes(List<Almacen> almacenes)
+    {
+        this.almacenes = almacenes;
+    }
+    
+     public void reset()
+    {
+        almacen = new Almacen();
+    }
+    public void register()
+    {
+        if (db.create(almacen))
+        {
+            almacen = new Almacen();
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro guardado exitosamente", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+            RequestContext con = RequestContext.getCurrentInstance();
+            con.execute("PF('registrar').hide();");
+        } else
+        {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Este registro no puede ser guardado", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+        }
+    }
+
+    //logica para eliminar un almacen
+    public void delete()
+    {
+        if (db.delete(almacen))
+        {
+            almacen = new Almacen();
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro eliminado exitosamente", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+            RequestContext con = RequestContext.getCurrentInstance();
+            con.execute("PF('eliminar').hide();");
+        } else
+        {
+             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_WARN, "Este registro no puede ser eliminado", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+        }
+    }
+    
+    public void update()
+    {
+        if(db.update(almacen))
+       {
+            almacen = new Almacen();
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registro modificado exitosamente", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+            RequestContext con = RequestContext.getCurrentInstance();
+            con.execute("PF('modificar').hide();");
+        }else
+        {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Este registro no puede ser modificado", null);
+            FacesContext.getCurrentInstance().addMessage("mensaje", mensaje);
+        }
+
+    }
+}
---
+++ src/main/java/com/pezfa/inventario/converter/CamionConverter.java
@@ -0,0 +1,55 @@
+package com.pezfa.inventario.converter;
+
+import com.pezfa.inventario.database.EmpleadoDB;
+import com.pezfa.inventario.models.Empleado;
+import java.util.List;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.view.Location;
+
+/**
+ *
+ * @author Alicia
+ */
+@FacesConverter(forClass = Location.class, value = "empleado")
+public class EmpleadoConverter implements Converter {
+
+    public static List<Empleado> empleados;
+
+    public EmpleadoConverter() {
+        EmpleadoDB db = new EmpleadoDB();
+        empleados = db.read("From empleado");
+    }
+
+    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+        Integer code =  Integer.parseInt(value);
+        Empleado obj = buscar(code);
+        return obj;
+        
+    }
+
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if(o==null||o.equals(""))
+        {
+            return "";
+        }else
+        {
+            return String.valueOf(((Empleado)o).getId());
+        }
+    }
+    
+    public Empleado buscar(int id)
+    {
+        for (Empleado obj : empleados)
+        {
+            if(obj.getId()==id)
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+
+}
