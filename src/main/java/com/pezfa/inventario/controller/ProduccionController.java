4d08f462b58d9b84b2cd4d6de33dd938855ae515
Carlos Cercado <cercadocarlos@gmail.com>
2017-07-09 17:35:03 -0400

    Trabajo de produccion

--- src/main/java/com/pezfa/inventario/controller/ProduccionController.java
+++ src/main/java/com/pezfa/inventario/controller/ProduccionController.java
@@ -1,12 +1,20 @@
 package com.pezfa.inventario.controller;
 
 import com.pezfa.inventario.database.ProduccionDB;
+import com.pezfa.inventario.database.UbicacionDB;
+import com.pezfa.inventario.database.UnidadDB;
 import com.pezfa.inventario.models.Compra;
 import com.pezfa.inventario.models.Produccion;
+import com.pezfa.inventario.models.ProductoSalida;
+import com.pezfa.inventario.models.Ubicacion;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+
 @ViewScoped
 @ManagedBean
 public class ProduccionController implements Serializable
@@ -15,13 +23,26 @@ public class ProduccionController implements Serializable
     Produccion produccion = null; // objeto a controlar
     List<Produccion> producciones = null; // lista de objetos de tipo almace
     private ProduccionDB db;
-    
-    
 
     public ProduccionController()
     {
         produccion = new Produccion(); //instancio el objeto almacen
         db = new ProduccionDB();
+    }
+    
+    public List<ProductoSalida> getInventario()
+    {
+        List<ProductoSalida> productos = new ArrayList<>();
+        UbicacionDB tdb = new UbicacionDB();
+        tdb.read("from Ubicacion uni join fetch uni.compraEspecie deta join"
+                + " fetch deta.especie esp where esp.cantidad > 0").stream()
+                    .distinct().forEach(x -> {
+                        x.setNombre(x.getCompraEspecie().getEspecie().getNombre());
+                        x.setCodigo(x.getCompraEspecie().getEspecie().getCodigo());
+                        x.setPrecio(x.getCompraEspecie().getEspecie().getPrecio());
+                        productos.add(x);
+                    });
+        return productos;
     }
 
     public Produccion getProduccion()
@@ -52,6 +73,12 @@ public class ProduccionController implements Serializable
         }
     }
 
+    public void onRowSelect(SelectEvent event)
+    {
+        RequestContext con = RequestContext.getCurrentInstance();
+        con.execute("PF('detalles').show();");
+    }
+
     public void delete()
     {
         if (db.delete(produccion))
@@ -75,6 +102,5 @@ public class ProduccionController implements Serializable
             System.out.println("No Actualizado");
         }
     }
-    
 
 }
